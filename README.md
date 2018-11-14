f91db9e69f15f31f3237581ccd59a19a667584ed
Fayvor George <phavorsparks@gmail.com>
2018-11-14 15:25:59 +0100

    install dependencies and add app.js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weatherify
+
 Learning to work with google API's... the geolocation api and NodeJs
+
+goto: https://maps.googleapis.com/maps/api/geocode/json
---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const request = require('request')
+
+request({
+  url: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.API_KEY}&address=1301%20lombard%20street%20philadelphia`,
+  json: true
+}, (error, response, body) => {
+  console.log(body)
+})
