395b1cdb55dffb1c23e48d7b4d5741e7ff64f179
Fayvor George <phavorsparks@gmail.com>
2018-11-14 17:23:18 +0100

    get yargs setup and setup dynamic location request from the terminal

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 require('dotenv').config()
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'address to fetch weather for',
+    string: true
+  }
+})
+ .help()
+ .alias('help', 'h')
+ .argv
+
+let address = encodeURI(argv.address)
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.API_KEY}&address=1301%20lombard%20street%20philadelphia`,
+  url: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.API_KEY}&address=${address}`,
   json: true
 }, (error, response, body) => {
   // console.log(JSON.stringify(body, undefined, 2)) // pretty printing
