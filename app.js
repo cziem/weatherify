cc59316e8e80ccf1b4c84f0baa3f08f11846b159
Fayvor George <phavorsparks@gmail.com>
2018-11-14 15:48:47 +0100

    API key finally works

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.API_KEY}&address=1301%20lombard%20street%20philadelphia`,
   json: true
 }, (error, response, body) => {
-  console.log(body)
+  try {
+    console.log(body)
+  } catch (e) {
+    if (e) {
+      console.log(error)
+    }
+  }
 })
