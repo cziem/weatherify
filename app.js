71b6374457aeead08b0b50af77168f53fd68e9ca
Fayvor George <phavorsparks@gmail.com>
2018-11-14 16:23:32 +0100

    pretty printing, and data extraction

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.API_KEY}&address=1301%20lombard%20street%20philadelphia`,
   json: true
 }, (error, response, body) => {
-  try {
-    console.log(body)
-  } catch (e) {
-    if (e) {
-      console.log(error)
-    }
-  }
+  // console.log(JSON.stringify(body, undefined, 2)) // pretty printing
+
+  console.log(`Address: ${body.results[0].formatted_address}`)
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 })
